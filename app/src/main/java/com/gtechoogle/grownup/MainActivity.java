3346a844f715b26d2f2d18b9af8b69ce93e00ccc
gtechoogle <gtechoogle@gmail.com>
2016-01-09 17:55:07 +0800

    1. add calendar view 2. add new grow activity

--- app/src/main/java/com/gtechoogle/grownup/MainActivity.java
+++ app/src/main/java/com/gtechoogle/grownup/MainActivity.java
@@ -1,5 +1,6 @@
 package com.gtechoogle.grownup;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,10 +13,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private CalendarView mCalendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +32,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                startNewGrownActivity();
             }
         });
 
@@ -40,7 +44,19 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        getView();
     }
+
+    private void startNewGrownActivity() {
+        Intent intent = new Intent(this, NewGrown.class);
+        startActivity(intent);
+    }
+
+    private void getView() {
+        mCalendarView = (CalendarView) findViewById(R.id.calendar_view);
+    }
+
 
     @Override
     public void onBackPressed() {
