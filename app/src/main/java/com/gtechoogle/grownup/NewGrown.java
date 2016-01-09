0cebe8e4f1b0b5ff145aa6a47ef841d3d7bded89
gtechoogle <gtechoogle@gmail.com>
2016-01-09 17:58:49 +0800

    1 add new grow activity

---
+++ app/src/main/java/com/gtechoogle/grownup/NewGrown.java
@@ -0,0 +1,13 @@
+package com.gtechoogle.grownup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewGrown extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_grown);
+    }
+}
