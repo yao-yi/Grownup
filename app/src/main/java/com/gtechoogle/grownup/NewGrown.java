fae084e76bc3fab7b60bb9cf97cd42ed9390f25d
gtechoogle <gtechoogle@gmail.com>
2016-01-09 18:23:02 +0800

    1. Add take photo function 2. Add record voice view 3. Add text view

--- app/src/main/java/com/gtechoogle/grownup/NewGrown.java
+++ app/src/main/java/com/gtechoogle/grownup/NewGrown.java
@@ -1,13 +1,47 @@
 package com.gtechoogle.grownup;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NewGrown extends AppCompatActivity {
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    View mCameraView;
+    View mVoiceView;
+    View mText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_grown);
+        getView();
+    }
+
+    private void getView() {
+        mCameraView = (View) findViewById(R.id.take_photo);
+        mCameraView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+        mVoiceView = (View) findViewById(R.id.record_voice);
+        mVoiceView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        mText = (View) findViewById(R.id.add_text);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 }
